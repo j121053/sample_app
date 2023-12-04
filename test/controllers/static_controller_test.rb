78ee6dea9b527e30aafffff01fbdedfef5b7b599
j121053 <152852260+j121053@users.noreply.github.com>
2023-12-04 13:50:29 +0000

    Add

--- test/controllers/static_controller_test.rb
+++ test/controllers/static_controller_test.rb
@@ -1,18 +1,22 @@
 require "test_helper"
 
-class StaticControllerTest < ActionDispatch::IntegrationTest
-  test "should get Pages" do
-    get static_Pages_url
-    assert_response :success
-  end
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   test "should get home" do
-    get static_home_url
+    get static_pages_home_url
     assert_response :success
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
-    get static_help_url
+    get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 end
