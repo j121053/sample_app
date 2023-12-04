8c87204bf069d86a9ec6948c87f3af4b858ba980
j121053 <152852260+j121053@users.noreply.github.com>
2023-12-04 12:38:53 +0000

    Exec

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,15 @@
+class StaticController < ApplicationController
+  def Pages
+  end
+
+  def home
+    # render ....
+    # => app/views/static_pages/home.html.erb
+  end
+
+  def help
+    # => app/views/static_pages/help.html.erb
+    # roots / root /rooting
+    # routes / route / routing
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'static/Pages'
+  get 'static/home'
+  get 'static/help'
   root "hello#index"
 end
